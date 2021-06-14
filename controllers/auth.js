425d8a4ea30f9ae1d250e4dda621877dd0c26b74
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-06-13 22:03:44 -0500

    141. login usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,63 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 
-const login = (req, res = response) => {
+const Usuario = require('../models/usuario');
 
-    res.json({
-        mensaje: 'Login OK'
+
+const login = async(req, res = response) => {
+    const { correo, password } = req.body;
+
+
+    let respuesta = { estado: 200, mensaje: "Login ok" };
+    try {
+        //verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            respuesta.estado = 400
+            respuesta.mensaje = 'Usuario / password no son correctos -correo'
+            return responder(res, respuesta)
+
+        }
+
+
+
+        //si el usuario está activo
+        if (!usuario.estado) {
+            respuesta.estado = 400
+            respuesta.mensaje = 'Usuario / password no son correctos -estado false'
+            return responder(res, respuesta)
+
+        }
+
+        //verificar la contraseña
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+        if (!validPassword) {
+            respuesta.estado = 400
+            respuesta.mensaje = 'Usuario / password no son correctos -password'
+            return responder(res, respuesta)
+
+        }
+
+        //generar jwt
+
+
+
+
+    } catch (error) {
+        res.status(500).json({
+            mensaje: 'Hable con el admin'
+        })
+    }
+
+
+}
+
+const responder = (res, respuesta) => {
+    console.log(respuesta)
+
+    res.status(respuesta.estado).json({
+        mensaje: respuesta.mensaje
     })
-
 }
 
 module.exports = {
